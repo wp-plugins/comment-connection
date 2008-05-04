248f34b629b046b5685a86ae14b77ad4116bf85b
wesg <wesg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-04 03:41:27 +0000

    Tagging version 1.3
    git-svn-id: https://plugins.svn.wordpress.org/comment-connection/tags/1.3@44162 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/Readme.txt
@@ -0,0 +1,46 @@
+=== Comment Connection ===
+Contributors: wesg
+Tags: comments,link,reply
+Requires at least: 2.0
+Tested up to: 2.5
+Stable tag: 1.3
+
+Comment Connection is a Wordpress plugin that automatically links comments as authors reply to each other.
+
+== Description ==
+
+Comment Connection is a plugin that turns an element of comment text to a link for a previous comment. Whenever an author uses @<i>commenter</i>, the text after @ is automatically linked to the previous comment it refers to.
+
+The plugin works by searching each comment for an occurance of @. Where it finds one, the plugin searches the comment database for a comment on the same post written by the person being referenced. By doing it this way, the plugin only adds one database query for each referencing comment. 
+
+If the type after the @ symbol does not correspond to a comment, no changes are made.
+
+Currently tested successfully with WP 2.5. If you have trouble with earlier versions, or even have it work properly on earlier versions, please let me know.
+
+USAGE
+
+1. Inside a comment, write @<i>commenter</i> where <i>commenter</i> is the name of the author you are replying to. It is preferable that this happens early in the comment.
+
+LIMITATIONS
+
+1. <i>Commenter</i> must be an exact match of the original author's name.
+2. Many database queries can occur for a post with many referencing comments.
+3. Only links to the last comment made by commenter (ie. if the commenter writes many comments before the @, the most recent one is linked to).
+
+
+== Installation ==
+
+1. Copy the folder comment-connection into your WordPress plugins directory (wp-content/plugins).
+2. Log in to WordPress Admin. Go to the Plugins page and click Activate for Comment Connection.
+
+
+== Frequently Asked Questions ==
+
+
+= What is the purpose of this plugin? =
+
+Comment Connection allows readers to follow conversations by automatically linking to comments that are replied to. This becomes especially important with longer conversations.
+
+= What symbol triggers the response? =
+
+The @ symbol currently modifies comments, though more options may become available later.
---
+++ trunk/comment-connection.php
@@ -0,0 +1,81 @@
+<?php
+/*
+Plugin Name: Comment connection
+Plugin URI: http://www.wesg.ca/2008/04/wordpress-plugin-comment-connection/
+Description: Link comments referencing one another automatically.
+Version: 1.3
+Author: Wes Goodhoofd
+Author URI: http://www.wesg.ca/
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of version 2 of the GNU General Public
+License as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details, available at
+http://www.gnu.org/copyleft/gpl.html
+or by writing to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+//plugin function
+function comment_connection($comment) {
+global $wpdb;
+
+//replace new lines \n with line breaks <br /> in order to detect comment reference
+$comment = nl2br($comment);
+
+//replace extra spaces after @
+$r = "@";
+$s = "@ ";
+$i = 1;
+$blank = substr_count($comment, $s);
+if ($blank > 0) 
+	$comment = str_replace($s, $r, $comment, $i);
+
+	//determine if someone is referencing a comment
+	$count = substr_count($comment, "@");
+
+	if ($count > 0) {
+
+	//find the post ID without printing to screen
+	$pid = get_the_ID();
+
+	//find the comment ID without printing to screen
+	$cid = get_comment_ID();
+	
+	//this is the big change
+	//determine all occurances of @ and their following authors
+	//then loads into array
+	preg_match_all("/@(.*)(:|\<br \/\>)/", $comment, $out);
+
+	for ($x=0; $x < count($out[1]); $x++) {
+		//solves little problem with colon usage
+		if (substr_count($out[1][$x], ':') > 0) 
+			$out[1][$x] = substr($out[1][$x], 0, strpos($out[1][$x], ':'));
+		//solves other little problem with extra spaces
+		if (substr_count($out[1][$x], ' ') <= 2)
+			$array[$x] = $out[1][$x];
+
+		//retrieve comment info from database
+		$d = db_query($pid, $cid, $array[$x]);
+
+		//replace the authors with their comment IDs
+		if ($d != NULL)
+			$comment = str_ireplace('@' . $array[$x], '@<a href=#comment-' . $d . '>' . $array[$x] . '</a>', $comment);
+	}
+}
+
+return $comment; //print the modified comments
+}
+
+function db_query($pid, $cid, $string) {
+	global $wpdb;
+	$result = $wpdb->get_var("SELECT comment_ID FROM $wpdb->comments WHERE comment_author = '$string' AND comment_post_ID = '$pid' AND comment_ID < '$cid' ORDER BY comment_date DESC LIMIT 1");
+
+	return $result;
+}
+
+add_filter('comment_text', 'comment_connection'); //apply before printing comments
